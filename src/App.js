ce94110b32b85048bb4756e3e1af5c2fccfac3c5
Alexander <alexander.botello@g.austincc.edu>
2019-02-13 17:50:16 -0600

    Add background image and font css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 
 import Image from "./components/Image";
-import Title from "./components/Title";
+// import Title from "./components/Title";
 import TextBody from "./components/TextBody";
 import "./App.css";
 
 function App() {
   return (
     <div className="Container">
-      <Image />
-      <Title />
       <TextBody />
+      <Image />
     </div>
   );
 }
--- src/components/TextBody/index.js
+++ src/components/TextBody/index.js
@@ -5,13 +5,9 @@ function TextBody() {
   return (
     <div className="Text">
       <p>
-        Hello! My name is Alexander Botello, and I'm an Austin based full-stack
-        web developer. I build web applications primarily with React, Node.js,
-        or Python.
-      </p>
-      <p>
-        I'm comfortable developing for both the front-end and back-end as well
-        as setting up and managing CI and testing pipelines.
+        Hello! I'm <span>Alexander Botello</span>
+        <br />
+        I'm a full-stack developer living in Austin.
       </p>
     </div>
   );
