4c5b9dfac63ecd257f146e20b3c656247463a051
Alexander <alexander.botello@g.austincc.edu>
2019-02-17 11:11:39 -0600

    Create Modal component

---
+++ src/components/Projects/Modal/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function Modal(props) {
+  return <div className="Modal">Image Project Name SubTitle Description</div>;
+}
+
+export default Modal;
