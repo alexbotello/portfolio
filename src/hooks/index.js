57ea5037a52dd59b88e398641c56986292d05c51
Alexander <alexander.botello@g.austincc.edu>
2019-02-21 12:41:40 -0600

    Refactor style and animation in Main

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -23,3 +23,18 @@ export const useHover = () => {
 
   return [ref, value];
 };
+
+export const isComponentInView = elementId => {
+  let el = document.getElementById(elementId);
+  let rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight ||
+        document.documentElement.clientHeight) /*or $(window).height() */ &&
+    rect.right <=
+      (window.innerWidth ||
+        document.documentElement.clientWidth) /*or $(window).width() */
+  );
+};
