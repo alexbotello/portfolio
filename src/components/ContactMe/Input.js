86ba63c8cdef23a2604bd7ce38a417f5f28a8b63
alexbotello <alexander.botello@g.austincc.edu>
2019-02-23 18:41:32 -0600

    Change default fonts for inputs

--- src/components/ContactMe/Input.js
+++ src/components/ContactMe/Input.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Input(props) {
-  return <input className="Input" type="text" placeholder={props.label} />;
+  return (
+    <input
+      className="Input"
+      type="text"
+      placeholder={props.label}
+      onChange={e => props.setValue(e.target.value)}
+    />
+  );
 }
 
 export default Input;
